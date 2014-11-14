daf34124e79c1d5bdd327164e1a2c3db3dde285e
AndyGee <andy.is@gmx.de>
2014-11-14 14:11:09 +0100

    Simplify

--- app/src/main/java/com/tomitribe/ee/ejb/EjbStateful.java
+++ app/src/main/java/com/tomitribe/ee/ejb/EjbStateful.java
@@ -23,8 +23,6 @@ import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.ejb.Stateful;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.function.IntBinaryOperator;
-import java.util.function.IntUnaryOperator;
 
 /**
  * EJB STEP 2 - The @javax.ejb.Stateful bean
@@ -53,42 +51,6 @@ public class EjbStateful implements IEjbStateful {
         return count.get();
     }
 
-    public boolean compareAndSet(final int expect, final int update) {
-        return count.compareAndSet(expect, update);
-    }
-
-    public double doubleValue() {
-        return count.doubleValue();
-    }
-
-    public int intValue() {
-        return count.intValue();
-    }
-
-    public float floatValue() {
-        return count.floatValue();
-    }
-
-    public int addAndGet(final int delta) {
-        return count.addAndGet(delta);
-    }
-
-    public int getAndDecrement() {
-        return count.getAndDecrement();
-    }
-
-    public byte byteValue() {
-        return count.byteValue();
-    }
-
-    public int getAndSet(final int newValue) {
-        return count.getAndSet(newValue);
-    }
-
-    public short shortValue() {
-        return count.shortValue();
-    }
-
     @Override
     public int incrementAndGet() {
         return count.incrementAndGet();
@@ -97,42 +59,6 @@ public class EjbStateful implements IEjbStateful {
     @Override
     public int decrementAndGet() {
         return count.decrementAndGet();
-    }
-
-    public int accumulateAndGet(final int x, final IntBinaryOperator accumulatorFunction) {
-        return count.accumulateAndGet(x, accumulatorFunction);
-    }
-
-    public boolean weakCompareAndSet(final int expect, final int update) {
-        return count.weakCompareAndSet(expect, update);
-    }
-
-    public int getAndAdd(final int delta) {
-        return count.getAndAdd(delta);
-    }
-
-    public void set(final int newValue) {
-        count.set(newValue);
-    }
-
-    public int getAndUpdate(final IntUnaryOperator updateFunction) {
-        return count.getAndUpdate(updateFunction);
-    }
-
-    public void lazySet(final int newValue) {
-        count.lazySet(newValue);
-    }
-
-    public int getAndAccumulate(final int x, final IntBinaryOperator accumulatorFunction) {
-        return count.getAndAccumulate(x, accumulatorFunction);
-    }
-
-    public int updateAndGet(final IntUnaryOperator updateFunction) {
-        return count.updateAndGet(updateFunction);
-    }
-
-    public long longValue() {
-        return count.longValue();
     }
 
     @Override
